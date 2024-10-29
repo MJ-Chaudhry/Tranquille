9f0cfd3dba8a1ae70514090783e286334b73998b
Brick Sigma <bricksigma176@gmail.com>
2024-10-29 08:25:04 +0300

    Created bottom naviagtion bar

---
+++ lib/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Home"),
+          Text("Other stuff"),
+          Text("More"),
+        ],
+      ),
+    );
+  }
+}
